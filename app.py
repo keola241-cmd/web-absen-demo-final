384ffa8ea4e21644b8288bdf8140594e33cfd0c0
keola241-cmd <keola241@gmail.com>
2026-09-05 20:23:31 +0700

    Refactor app.py for improved readability

--- app.py
+++ app.py
@@ -8,7 +8,6 @@ app = Flask(__name__)
 SUPABASE_URL = os.environ.get("SUPABASE_URL")
 SUPABASE_KEY = os.environ.get("SUPABASE_KEY")
 
-# Inisialisasi client Supabase jika Environment Variable tersedia
 supabase: Client = None
 if SUPABASE_URL and SUPABASE_KEY:
     supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
